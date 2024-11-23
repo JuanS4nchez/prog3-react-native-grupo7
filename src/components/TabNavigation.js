e769323f9709c8ff3c4e46308b8251d783976e6c
Chino Menin <chinomenin@MacBook-Air-de-Chino.local>
2024-11-23 15:30:13 -0300

    navigation (falta agregat algun tab y ] la ruta)

---
+++ src/components/TabNavigation.js
@@ -0,0 +1,40 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Home from "../screens/Home";
+import Entypo from "@expo/vector-icons/Entypo";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import AntDesign from '@expo/vector-icons/AntDesign';
+
+
+const Tab = createBottomTabNavigator();
+const TabNavigation = () => {
+  return (
+    <Tab.Navigator screenOptions={{ tabBarShowLabel: false }}>
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarIcon: () => <Entypo name="home" size={24} color="black" />,
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarIcon: () => (
+            <Ionicons name="person-circle" size={24} color="black" />
+          ),
+        }}
+      />
+       <Tab.Screen
+        name="NuevoPost"
+        component={NuevoPost}
+        options={{
+          tabBarIcon: () => <AntDesign name="pluscircle" size={24} color="black" />,
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
+
+
+export default TabNavigation;
