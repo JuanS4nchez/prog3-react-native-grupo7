1f9b2ee23359906a1e481efde9e6a430e16df9cc
Juan Eustaquio Vergara del Pozo Ferrucci <vergaraj@udesa.edu.ar>
2024-11-25 09:56:04 -0300

    Filter

---
+++ src/components/Users.js
@@ -0,0 +1,81 @@
+import {
+    Text,
+    View,
+    StyleSheet,
+    FieldValue,
+    TouchableOpacity,
+  } from "react-native";
+  import React, { Component } from "react";
+  import { auth, db } from "../firebase/config";
+  import AntDesign from "@expo/vector-icons/AntDesign";
+  import AntDesign1 from "@expo/vector-icons/AntDesign";
+  import firebase from "firebase";
+  
+  export default class Users extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        posts: [],
+     };
+    }
+
+    componentDidMount() {
+      db.collection("posts")
+        .where("email", "==", this.props.user.data.email)
+        .onSnapshot((docs) => {
+          let currentPosts = [];
+          docs.forEach((doc) => {
+            currentPosts.push({
+              id: doc.id,
+              data: doc.data(),
+            });
+          });
+          this.setState({
+            posts: currentPosts,
+            loading: false,
+          });
+        });
+    }
+
+
+  
+    render() {
+      return (
+        <View style={styles.container}>
+          <View style={styles.postContainer}>
+            <Text style={styles.email}>{this.props.user.data.email}</Text>
+            <Text style={styles.message}>{this.props.user.data.userName}</Text>
+            <Text style={styles.message}>Cantidad de posts: {this.state.posts.length}</Text>
+          </View>
+        </View>
+      );
+    }
+  }
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      padding: 10,
+      backgroundColor: "#f8f9fa",
+    },
+    postContainer: {
+      backgroundColor: "#ffffff",
+      borderRadius: 8,
+      padding: 15,
+      marginBottom: 10,
+      shadowColor: "#000",
+    },
+    email: {
+      fontSize: 16,
+      fontWeight: "bold",
+      color: "#333",
+    },
+    message: {
+      fontSize: 14,
+      color: "#555",
+      marginTop: 5,
+    },
+  });
+
+
+  
--- src/screens/Search.js
+++ src/screens/Search.js
@@ -1,11 +1,101 @@
+import {
+  Text,
+  View,
+  StyleSheet,
+  FieldValue,
+  TouchableOpacity,
+  FlatList,
+  TextInput
+} from "react-native";
 import React, { Component } from "react";
+import { auth, db } from "../firebase/config";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import AntDesign1 from "@expo/vector-icons/AntDesign";
+import firebase from "firebase";
+import Users from "../components/Users";
+
 export default class Profile extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      users: [],
+      buscador: "",
+      usersFiltrados: []
+    };
+  }
+
+  componentDidMount() {
+    db.collection("users")
+      .onSnapshot((docs) => {
+        let users = [];
+        docs.forEach((doc) => {
+          users.push({
+            id: doc.id,
+            data: doc.data(),
+          });
+        });
+        this.setState(
+          {
+            usersFiltrados: users,
+            users: users,
+            loading: false,
+          },
+          () => console.log(this.state.users)
+        );
+      });
+  }
+
+  onHandleFilter(text){
+    const userValue = text;
+      this.setState({
+      buscador: userValue,
+      usersFiltrados: this.state.users.filter((user) => user.data.email.toLowerCase().includes(userValue.toLowerCase()))
+    })
   }
 
   render() {
-    return;
+    console.log(this.state.usersFiltrados, "ARBOL")
+    return(
+    <View style= {styles.container}>
+      <TextInput
+          style={styles.input}
+          placeholder="buscar..."
+          onChangeText={(text)=> this.onHandleFilter(text)}
+          value={this.state.buscador}
+        />
+      {this.state.usersFiltrados.length === 0 ? (
+          <Text style= {styles.notFound}> La cuenta con email "{this.state.buscador}" no existe</Text>
+        ) 
+        : <FlatList
+        data={this.state.usersFiltrados}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => <Users user={item} />}
+        /> }     
+    </View>
+    )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 10,
+    marginTop: 20,
+    flex: 1,
+  },
+  input: {
+    height: 20,
+    paddingVertical: 15,
+    paddingHorizontal: 10,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderStyle: "solid",
+    borderRadius: 6,
+    marginVertical: 10,
+  },
+  notFound: {
+    fontSize: 15,
+    fontWeight: "bold",
+    marginBottom: 10,
+    color: "#333",
+  }
+});
